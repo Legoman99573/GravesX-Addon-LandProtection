327f864d74f4577583d6ef4669c934dfc466a55f
Christian W. <7460704+Legoman99573@users.noreply.github.com>
2025-08-20 14:01:16 -0500

    Fix command causing exception

--- src/main/java/dev/cwhead/GravesXAddon/commands/LandProtectionCommand.java
+++ src/main/java/dev/cwhead/GravesXAddon/commands/LandProtectionCommand.java
@@ -2,7 +2,10 @@ package dev.cwhead.GravesXAddon.commands;
 
 import dev.cwhead.GravesXAddon.LandProtection;
 import org.bukkit.ChatColor;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -12,12 +15,21 @@ import java.util.List;
 
 public class LandProtectionCommand implements CommandExecutor, TabCompleter {
 
-    private static final String PERM = "gravesx.landprotection.commands";
+    private static final String PERM  = "gravesx.landprotection.commands";
     private static final String DEBUG = "gravesx.landprotection.debug";
+
     private final LandProtection plugin;
 
     public LandProtectionCommand(LandProtection plugin) {
         this.plugin = plugin;
+    }
+
+    private boolean hasPerm(CommandSender sender, String perm) {
+        if (sender instanceof Player) {
+            Player p = ((Player) sender);
+            return plugin.getGravesXAPI().getGravesX().hasGrantedPermission(perm, p);
+        }
+        return true;
     }
 
     @Override
@@ -25,32 +37,34 @@ public class LandProtectionCommand implements CommandExecutor, TabCompleter {
                              @NotNull Command command,
                              @NotNull String label,
                              @NotNull String[] args) {
+
         if (args.length == 0 || !"reload".equalsIgnoreCase(args[0])) {
-            if ((sender instanceof ConsoleCommandSender) && plugin.getGravesXAPI().getGravesX().hasGrantedPermission(DEBUG, (Player) sender)) {
-                sender.sendMessage(ChatColor.RED + "GravesXAddon-LandProtection Debug Information");
-                sender.sendMessage(ChatColor.RED + "GriefPrevention: " + (plugin.isGriefPreventionEnabled() ? ChatColor.GREEN + "listening" : ChatColor.RED + "not listening"));
-                sender.sendMessage(ChatColor.RED + "GriefDefender: " + (plugin.isGriefDefenderEnabled() ? ChatColor.GREEN + "listening" : ChatColor.RED + "not listening"));
-                sender.sendMessage(ChatColor.RED + "Lands: " + (plugin.isLandsEnabled() ? ChatColor.GREEN + "listening" : ChatColor.RED + "not listening"));
-                sender.sendMessage(ChatColor.RED + "Towny: " + (plugin.isTownyEnabled() ? ChatColor.GREEN + "listening" : ChatColor.RED + "not listening"));
-                sender.sendMessage(ChatColor.RED + "WorldGuard: " + (plugin.isWorldGuardEnabled() ? ChatColor.GREEN + "listening" : ChatColor.RED + "not listening"));
+            if (!hasPerm(sender, DEBUG)) {
+                sender.sendMessage(ChatColor.RED + "☠ You don't have permission to do that.");
                 return true;
             }
-            sender.sendMessage(ChatColor.RED + "You don't have permission to do that.");
+
+            sender.sendMessage(ChatColor.RED + "☠ " + ChatColor.GOLD + "GravesXAddon-LandProtection Debug Information");
+            sender.sendMessage(ChatColor.GOLD + "GriefPrevention: " + (plugin.isGriefPreventionEnabled() ? ChatColor.GREEN + "Hooked" : ChatColor.RED + "Not Hooked"));
+            sender.sendMessage(ChatColor.GOLD + "GriefDefender: " + (plugin.isGriefDefenderEnabled() ? ChatColor.GREEN + "Hooked" : ChatColor.RED + "Not Hooked"));
+            sender.sendMessage(ChatColor.GOLD + "Lands: " + (plugin.isLandsEnabled() ? ChatColor.GREEN + "Hooked" : ChatColor.RED + "Not Hooked"));
+            sender.sendMessage(ChatColor.GOLD + "Towny: " + (plugin.isTownyEnabled() ? ChatColor.GREEN + "Hooked" : ChatColor.RED + "Not Hooked"));
+            sender.sendMessage(ChatColor.GOLD + "WorldGuard: " + (plugin.isWorldGuardEnabled() ? ChatColor.GREEN + "Hooked" : ChatColor.RED + "Not Hooked"));
             return true;
         }
 
-        if (!(sender instanceof ConsoleCommandSender) && !plugin.getGravesXAPI().getGravesX().hasGrantedPermission(PERM, (Player) sender)) {
-            sender.sendMessage(ChatColor.RED + "You don't have permission to do that.");
+        if (!hasPerm(sender, PERM)) {
+            sender.sendMessage(ChatColor.RED + "☠ You don't have permission to do that.");
             return true;
         }
 
         try {
             plugin.reloadAllConfigs();
         } catch (Throwable ignored) {
-            //ignored
+            // ignored
         }
 
-        sender.sendMessage(ChatColor.GREEN + "GravesX Land Protection: configs reloaded.");
+        sender.sendMessage(ChatColor.RED + "☠ " + ChatColor.GREEN + "GravesX Land Protection: configs reloaded.");
         return true;
     }
 
@@ -60,11 +74,12 @@ public class LandProtectionCommand implements CommandExecutor, TabCompleter {
                                                 @NotNull String alias,
                                                 @NotNull String[] args) {
         if (args.length == 1) {
-            if (!(sender instanceof ConsoleCommandSender) && !plugin.getGravesXAPI().getGravesX().hasGrantedPermission(PERM, (Player) sender))
-                return Collections.emptyList();
-
-            if ("reload".startsWith(args[0].toLowerCase()))
-                return Collections.singletonList("reload");
+            if (hasPerm(sender, PERM)) {
+                String partial = args[0].toLowerCase();
+                if ("reload".startsWith(partial)) {
+                    return Collections.singletonList("reload");
+                }
+            }
         }
         return Collections.emptyList();
     }
